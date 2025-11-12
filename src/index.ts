b33275a9eca24523e608171bd04c8572281e8957
Devon Daviau <devondaviau@yahoo.ca>
2025-11-12 08:57:21 -0500

    convert typeDefs.ts to schema.graphql

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
+import { readFileSync } from "fs";
 
-import typeDefs from "./schema/typeDefs.js";
 import resolvers from "./schema/resolvers.js";
+
+const typeDefs = readFileSync("./src/schema/schema.graphql", {
+  encoding: "utf-8",
+});
 
 const server = new ApolloServer({
   typeDefs,
