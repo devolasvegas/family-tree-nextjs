1f2d263588db09e2d16c37b17c3c0928f9832feb
Devon Daviau <devondaviau@yahoo.ca>
2025-11-13 12:27:20 -0500

    update resolvers to add locations resolver and function for mapping database row names to graphql names

--- src/schema/resolvers.ts
+++ src/schema/resolvers.ts
@@ -1,28 +1,85 @@
 import { Pool } from "pg";
 const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 
+const mapPersonRows = (row) => ({
+  id: row.id,
+  firstName: row.first_name,
+  lastName: row.last_name,
+  gender: row.gender,
+  birthDate: row.birth_date,
+  deathDate: row.death_date,
+  birthLocation: row.birthLocation,
+  deathLocation: row.deathLocation,
+  notes: row.notes,
+  createdBy: row.created_by,
+  createdAt: row.created_at,
+  parents: row.parents,
+  children: row.children,
+  spouses: row.spouses,
+});
+
 const resolvers = {
+  Query: {
+    async person(_, { id }) {
+      const { rows } = await pool.query("SELECT * FROM persons WHERE id = $1", [
+        id,
+      ]);
+      return mapPersonRows(rows[0]);
+    },
+
+    async persons() {
+      const { rows } = await pool.query("SELECT * FROM persons");
+      return rows.map(mapPersonRows);
+    },
+  },
+
   Person: {
     // All these are computed from the relationships table
 
-    async parents(person, _, __) {
+    async birthLocation(person, _, __) {
       const { rows } = await pool.query(
-        `SELECT p.* FROM persons p
-         JOIN relationships r ON p.id = r.related_person_id
-         WHERE r.person_id = $1 AND r.type = 'child'`,
+        `SELECT l.* from locations l
+         JOIN persons p ON p.birth_location_id = l.id
+         WHERE p.id = $1
+        `,
         [person.id]
       );
-      return rows;
+
+      return rows[0];
     },
 
-    async children(person, _, __) {
+    async deathLocation(person, _, __) {
+      const { rows } = await pool.query(
+        `SELECT l.* from locations l
+         JOIN persons p ON p.death_location_id = l.id
+         WHERE p.id = $1
+        `,
+        [person.id]
+      );
+
+      return rows[0];
+    },
+
+    async parents(person, _, __) {
       const { rows } = await pool.query(
         `SELECT p.* FROM persons p
          JOIN relationships r ON p.id = r.related_person_id
          WHERE r.person_id = $1 AND r.type = 'parent'`,
         [person.id]
       );
-      return rows;
+
+      return rows.map(mapPersonRows);
+    },
+
+    async children(person, _, __) {
+      const { rows } = await pool.query(
+        `SELECT p.* FROM persons p
+         JOIN relationships r ON p.id = r.related_person_id
+         WHERE r.person_id = $1 AND r.type = 'child'`,
+        [person.id]
+      );
+
+      return rows.map(mapPersonRows);
     },
 
     async spouses(person, _, __) {
@@ -32,7 +89,8 @@ const resolvers = {
          WHERE r.person_id = $1 AND r.type = 'spouse'`,
         [person.id]
       );
-      return rows;
+
+      return rows.map(mapPersonRows);
     },
   },
 };
