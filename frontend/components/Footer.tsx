1a2e9b33dec499deb63db8c2a2a3458247b2298a
Devon Daviau <devondaviau@yahoo.ca>
2025-11-14 11:45:31 -0500

    add new config files and package.json to start setup for pnpm and turbo

--- frontend/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <footer className="w-full border-t bg-gray-100 py-4 text-center text-sm text-gray-500">
-      © {new Date().getFullYear()} Family Tree. All rights reserved.
-    </footer>
-  );
-};
-
-export default Footer;
--- frontend/providers/SessionWrapper.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { SessionProvider } from "next-auth/react";
-
-export function SessionWrapper({ children }: React.PropsWithChildren) {
-  return <SessionProvider>{children}</SessionProvider>;
-}
