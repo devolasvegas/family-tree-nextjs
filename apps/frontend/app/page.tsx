6f04c571024e1a93a599cd51a3b3f588c9bb844a
Devon Daviau <devondaviau@yahoo.ca>
2026-04-28 15:12:41 -0400

    frontend: add new image and banner/masthead to home page

--- apps/frontend/app/page.tsx
+++ apps/frontend/app/page.tsx
@@ -31,7 +31,15 @@ export default async function Home() {
 
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+      <main className="flex min-h-screen w-full max-w-7xl flex-col items-center justify-between margin-x-auto bg-white dark:bg-black sm:items-start">
+        <div className="relative h-[500px] w-full">
+          <Image
+            src="/rajiv-perera-_JjYYsQPneE-unsplash.jpg"
+            alt="A photo of an extended family posing together in front of a white house."
+            fill
+            className="object-cover object-center"
+          />
+        </div>
         <Image
           className="dark:invert"
           src="/next.svg"
