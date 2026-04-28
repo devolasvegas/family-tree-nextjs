2210f7c1e2ce274c436304292430654c316ce393
Devon Daviau <devondaviau@yahoo.ca>
2026-04-28 10:30:58 -0400

    frontend: commit codegen file and generated directory

---
+++ apps/frontend/codegen.ts
@@ -0,0 +1,19 @@
+import { type CodegenConfig } from "@graphql-codegen/cli";
+
+const config: CodegenConfig = {
+  schema: "http://localhost:4000/",
+  documents: ["./app/**/*.tsx", "./app/**/*.ts", "./components/**/*.tsx"],
+  generates: {
+    "./__generated__/": {
+      preset: "client",
+      presetConfig: {
+        gqlTagName: "gql",
+      },
+    },
+    "./__generated__/types.ts": {
+      plugins: ["typescript", "typescript-operations"],
+    },
+  },
+};
+
+export default config;
